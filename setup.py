d2b8488e8470c42dd7cfd1c2ebe6709668cdbad5
Sergio Romero Barra <s.romerobarra@gmail.com>
2016-08-10 13:54:58 +0200

    Added pypi config.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'pdc',
+  packages = ['pdc'],
+  version = '1.1.2',
+  description = 'Python Device Communication.',
+  author = 'Sergio Romero Barra',
+  author_email = 's.romerobarra@gmail.com',
+  url = 'https://github.com/sergiorb/pdc',
+  download_url = 'https://github.com/sergiorb/sypi/tarball/1.1.2',
+  keywords = ['arduino', 'serial', 'communication'],
+  classifiers = [],
+)
