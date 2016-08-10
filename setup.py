6386903b4fffa79dab47869f0d993f4cd711f39b
Sergio Romero Barra <s.romerobarra@gmail.com>
2016-08-10 20:12:43 +0200

    migrated to setuptools

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
-  name = 'pdc',
-  packages = ['pdc'],
-  version = '1.1.5',
-  description = 'Python Device Communication.',
-  author = 'Sergio Romero Barra',
-  author_email = 's.romerobarra@gmail.com',
-  url = 'https://github.com/sergiorb/pdc',
-  download_url = 'https://github.com/sergiorb/pdc/tarball/1.1.5',
-  keywords = ['arduino', 'serial', 'communication'],
-  classifiers = [],
+  name = "pdc",
+  version = "1.1.6",
+  packages = find_packages(),
+  install_requires = ['pyserial>=3.0.1'],
+  package_data = {
+    '': ['*.txt', '*.rst', '*.md',],
+  },
+
+  # metadata for upload to PyPI
+  author = "Sergio Romero Barra",
+  author_email = "s.romerobarra@gmail.com",
+  description = "Python Device Communication.",
+  license = "MIT",
+  keywords = "arduino serial communication",
+  url = "https://github.com/sergiorb/pdc",
+  download_url = 'https://github.com/sergiorb/pdc/tarball/1.1.6'
 )
