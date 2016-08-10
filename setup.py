17a3e7666596b889fdd56e4acd46cc32256180d9
Sergio Romero Barra <s.romerobarra@gmail.com>
2016-08-10 15:40:43 +0200

    New version for reuploading to pypi.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'pdc',
   packages = ['pdc'],
-  version = '1.1.4',
+  version = '1.1.5',
   description = 'Python Device Communication.',
   author = 'Sergio Romero Barra',
   author_email = 's.romerobarra@gmail.com',
   url = 'https://github.com/sergiorb/pdc',
-  download_url = 'https://github.com/sergiorb/pdc/tarball/1.1.4',
+  download_url = 'https://github.com/sergiorb/pdc/tarball/1.1.5',
   keywords = ['arduino', 'serial', 'communication'],
   classifiers = [],
 )
